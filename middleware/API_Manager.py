83b51424ab5de5e421203ff23fa93d9d08a4fa9c
Josef Sikula <joza.sikula@gmail.com>
2024-02-17 12:08:28 +0100

    cislosloky

--- middleware/API_Manager.py
+++ middleware/API_Manager.py
@@ -2,21 +2,36 @@ import json
 from datetime import datetime
 
 
-class API_Manager(object):
+class API_Manager(object):  
 
     def __init__(self) -> None:
+        self.cisloSloky = 0
         pass
 
-    def sendRequest(self, request):
+    def sendRequest(self, request): #creates json file
         timestamp = datetime.now()
         dictionary = {
             "action":request,
             "timestamp":str(timestamp)
         }
         json_object = json.dumps(dictionary, indent=2)
-        with open("../tmp/request_"+timestamp.strftime("%Y-%m-%d_%I-%M-%S_%f")+".json", "w") as outfile:
+        with open("../tmp/log/request_"+timestamp.strftime("%Y-%m-%d_%I-%M-%S_%f")+".json", "w") as outfile:
             outfile.write(json_object)
+        with open("../tmp/sloka/cisloSloky.txt", "w") as outfileCisloSloky:
+
+            outfileCisloSloky.write(self.computeCisloSloky(request))
         pass
+
+    def computeCisloSloky(self, request):
+        if(request == "down"):
+            if (self.cisloSloky >0):
+                self.cisloSloky -=1
+
+        if(request == "up"):
+                if (self.cisloSloky <6):
+                    self.cisloSloky +=1
+
+
 
 if __name__ == "__main__":
     print(datetime.now())
