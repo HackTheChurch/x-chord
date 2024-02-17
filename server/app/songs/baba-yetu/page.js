9149ae3440de0aa715c273fb4c1df11e9612e794
petrkucerak <petr@khome.cz>
2024-02-17 11:50:34 +0100

    Create action to move into all page

--- server/app/songs/baba-yetu/page.js
+++ server/app/songs/baba-yetu/page.js
@@ -9,7 +9,7 @@ import { useEffect } from "react";
 export default function Home() {
   useEffect(() => {
     let parts = new Array();
-    for (let i = 0; i < 6; i += 1) {
+    for (let i = 0; i < 7; i += 1) {
       const part = document.querySelector(`#part-${i}`);
       parts.push(part);
     }
@@ -26,7 +26,7 @@ export default function Home() {
             switch (instruction.action) {
               case "down":
                 position += 1;
-                position > 6 ? (position = 5) : null;
+                position > 7 ? (position = 6) : null;
                 break;
 
               case "up":
@@ -41,13 +41,15 @@ export default function Home() {
         .catch((error) => {
           console.error("Error fetching file:", error);
         });
-    }, 500);
+    }, 250);
   });
   return (
     <>
       <Header />
       <Main>
-        <h1 className="text-xl font-bold uppercase mt-12">Baba yetu</h1>
+        <h1 className="text-xl font-bold uppercase mt-12" id="part-0">
+          Baba yetu
+        </h1>
         <div className="mt-2">
           <span className="flex items-center">
             <IconAuthor className={"mr-2"} /> Christopher Tin
@@ -57,7 +59,7 @@ export default function Home() {
           </span>
         </div>
         <div className="mt-4 mb-8 text-xl leading-8">
-          <p className="py-4" id="part-0">
+          <p className="py-4" id="part-1">
             R: Baba yetu, yetu uliye
             <br />
             Mbinguni yetu, yetu, amina!
@@ -75,7 +77,7 @@ export default function Home() {
             <br />
             Jina lako litukuzwe
           </p>
-          <p className="mb-2 pt-12" id="part-1">
+          <p className="mb-2 pt-12" id="part-2">
             1. Utupe leo chakula chetu
             <br />
             Tunachohitaji utusamehe
@@ -93,7 +95,7 @@ export default function Home() {
             <br />
             Mwovu, milele!
           </p>
-          <p className="mb-2 pt-12" id="part-2">
+          <p className="mb-2 pt-12" id="part-3">
             R: Baba yetu, yetu uliye
             <br />
             Mbinguni yetu, yetu, amina!
@@ -111,7 +113,7 @@ export default function Home() {
             <br />
             Jina lako litukuzwe
           </p>
-          <p className="mb-2 pt-12" id="part-3">
+          <p className="mb-2 pt-12" id="part-4">
             B: Ufalme wako ufike utakalo
             <br />
             Lifanyike duniani kama mbinguni. (Amina)
@@ -134,7 +136,7 @@ export default function Home() {
             <br />
             Jina lako litukuzwe
           </p>
-          <p className="mb-2 pt-12" id="part-5">
+          <p className="mb-2 pt-12" id="part-6">
             1. Utupe leo chakula chetu
             <br />
             Tunachohitaji utusamehe
