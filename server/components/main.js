599a7bd4e9c6281498aaa2d9f266bf587153898c
petrkucerak <petr@khome.cz>
2024-02-17 09:36:43 +0100

    Create set min h

--- server/components/main.js
+++ server/components/main.js
@@ -1,3 +1,7 @@
 export default function Main({ children }) {
-  return <main className="max-w-[1000px] w-[90vw]">{children}</main>;
+  return (
+    <main className="max-w-[1000px] w-[90vw] flex flex-col items-start min-h-[70vh]">
+      {children}
+    </main>
+  );
 }
