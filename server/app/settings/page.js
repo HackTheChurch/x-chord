fc4c6bbc595eb4dd2790edce1283d9c9d3ffb84a
petrkucerak <petr@khome.cz>
2024-02-17 03:27:27 +0100

    Create title in settings partition

--- server/app/settings/page.js
+++ server/app/settings/page.js
@@ -10,6 +10,10 @@ export default function Home() {
       <Main>
         <h1 className="text-xl font-bold uppercase">Nastavení</h1>
         <p>Tato stránka umožňuje základní konfiguraci.</p>
+        <div>
+          <h2 className="text-lg font-bold mt-2">Spuštěné služby</h2>
+          <input></input>
+        </div>
       </Main>
       <Footer />
     </>
